b5d37c3f7a3f7c7838dd8c22249c5a07204c0c49
niais <wang_minsi@163.com>
2016-07-03 23:09:20 +0800

    feature extraction

--- Group_given_tracklets/main_group.m
+++ Group_given_tracklets/main_group.m
@@ -11,7 +11,9 @@ num_video = length(total_video_bbs);
 
 
 % for it = 1:num_video
-for it = 44
+axis_xyz = {};
+track_graph = {};
+for it = 1:44
     % init info
     name_track = tracklist(it).name;
     num_frame = total_video_bbs(it).total_frame_num;
@@ -19,24 +21,26 @@ for it = 44
     
     tracklet = readTracks([track_root name_track]);
     
-    % tracklets matrix
+    %% tracklets matrix
     % show track in spatial-temporal space
     num_person = length(tracklet);
     [coordinary,mat_track] = STTracks(tracklet,num_frame);
     
 %     showTracks(path_video,tracklet);
 
-    % generate group-similarity between different people
-    d_alpha = 5e-4;
-    v_beta = 5e-4;
-    p_gamma = 1e-3;
-    [map_group,map_distance,map_velocity,map_position] =...
-        groupSimi(num_person,coordinary,tracklet,d_alpha,v_beta,p_gamma);
+    %% generate group-similarity between different people
+%     d_alpha = 5e-4;
+%     v_beta = 5e-4;
+%     p_gamma = 1e-3;
+%     [map_group,map_distance,map_velocity,map_position] =...
+%         groupSimi(num_person,coordinary,tracklet,d_alpha,v_beta,p_gamma);
+%     
+%     assign_group0 = double(map_group>0.5&map_group<5);
+%     assign_group1 = double(map_position<0.5&map_position>=0);
     
-    assign_group0 = double(map_group>0.5&map_group<5);
-    assign_group1 = double(map_position<0.5&map_position>=0);
-    
+    axis_xyz = [axis_xyz {coordinary}];
+    track_graph = [track_graph mat_track];
 %     assign_group = assign_group1;
 %     save([save_root total_video_bbs(it).video_name],'assign_group');
 end
-
+save('personlet_info.mat','axis_xyz','track_graph');
